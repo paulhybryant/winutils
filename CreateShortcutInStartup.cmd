20fd7fa7e0e81718d08047cfb9eab45b25c8e7ae
paulhybryant <paulhybryant@gmail.com>
2014-11-23 11:42:37 -0800

    Rename

--- CreateShortcutInStartup.cmd
+++ CreateShortcutInStartup.cmd
@@ -1,6 +1,6 @@
 @echo off
 :: change this to whatever you need
-set targetfolder=C:\Users\Yu\AppData\Roaming\Microsoft\Windows\SendTo
+set targetfolder=D:\Users\Yu\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
 :: If you don't want the extension, change the following to 
 shortcut /f:"%targetfolder%\%~n1.lnk" /a:c /t:%1
 :: shortcut /f:"%targetfolder%\%~n1%~x1.lnk" /a:c /t:%1
